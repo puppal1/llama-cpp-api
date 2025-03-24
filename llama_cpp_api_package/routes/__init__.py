3b52020d88427f86f598927326480ef38771c8a4
puppal1 <prish.uppal@gmail.com>
2025-03-24 16:27:52 -0700

    Remove redundant routes directory, as all functionality has been migrated to routes2

--- llama_cpp_api_package/routes/__init__.py
+++
@@ -1,5 +0,0 @@
-from .model_routes import router as model_router
-from .chat_routes import router as chat_router
-from .metrics_routes import router as metrics_router
-
-__all__ = ['model_router', 'chat_router', 'metrics_router'] 
